5ed4249d587c90778d80d4de0813417496466571
shell02 <92311244+shell02@users.noreply.github.com>
2022-05-30 21:43:57 +0200

    Update player.py

--- player.py
+++ player.py
@@ -2,10 +2,12 @@ import math
 
 class Player:
     VEL = 4
+    STAG = 0
 
     def __init__(self, Rect, img):
         self.rect = Rect
         self.img = img
+        self.lane = self.getLane()
 
     def collision(self, cars):
         for car in cars:
@@ -19,11 +21,23 @@ class Player:
         if keys_pressed == 2 and \
                 self.rect.x + self.VEL + self.rect.width < 500:
             self.rect.x += self.VEL # got to the right
+         if self.lane != self.getLane():
+            self.lane = self.getLane()
+            self.STAG = 0
+        else:
+            self.STAG += 1
 
     def draw(self, win):
          win.blit(self.img, (self.rect.x, self.rect.y))
     
-    def getDistance(self, car):
-        a = (self.rect.x + 40, self.rect.y)
-        b = (car.rect.x + 40, car.rect.y + car.rect.height)
-        return int(math.dist(a, b))
+    def getLane(self):
+        if self.rect.x < 110:
+            return 0
+        if 110 <= self.rect.x < 210:
+            return 1
+        if 210 <= self.rect.x < 310:
+            return 2
+        if 310 <= self.rect.x < 410:
+            return 3
+        if 400 <= self.rect.x:
+            return 4
